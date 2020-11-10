bea8a716ac0183f4d5adb2a2f1f5d7bf14bae1aa
joe.wright <joe@joejag.com>
2020-11-10 23:11:39 +0000

    typescript example

---
+++ available/ts/src/__tests__/main.spec.ts
@@ -0,0 +1,5 @@
+import { isFoo } from '../main'
+
+test('isFoo(3, 7) is 10', () => {
+  expect(isFoo(3, 7)).toBe(10)
+})
---
+++ available/ts/src/main.ts
@@ -0,0 +1,5 @@
+const isFoo = (a: number, b:number): number => {
+  return a + b
+}
+
+export { isFoo }
