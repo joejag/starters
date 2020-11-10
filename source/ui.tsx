0c536e076f1c4c111720b3144e15d1218a22469e
joe.wright <joe@joejag.com>
2020-11-10 22:00:05 +0000

    Handle error copying folder

--- source/ui.tsx
+++ source/ui.tsx
@@ -1,23 +1,31 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import { Text } from 'ink'
 import fs from 'fs-extra'
 
 const App: FC<{language?: string}> = ({ language = 'node' }) => {
+  const [status, setStatus] = useState({ done: false, error: null })
+
   const source = `available/${language}`
   const destination = `${process.cwd()}/${language}_project`
 
-  fs.copy(source, destination, function (err) {
-    if (err) {
-      console.log('An error occured while copying the folder.')
-      return console.error(err)
-    }
-    console.log('Copy completed!')
-  })
+  useEffect(() => {
+    fs.copy(source, destination, (err: Error) => {
+      if (err) {
+        setStatus({ done: false, error: err })
+        console.error(err)
+      } else {
+        setStatus({ done: true, error: null })
+      }
+    })
+  }, [source, language])
 
   return (
-    <Text>
-      Hello, <Text color='green'>{destination}</Text>
-    </Text>
+    <>
+      {!status.done && !status.error && <Text>Copying <Text color='yellow'>{language}</Text> project</Text>}
+      {status.error && <Text>Something went wrong copying to <Text color='red'>{destination}</Text></Text>}
+      {status.done &&
+        <Text>Copied <Text color='yellow'>{language}</Text> to <Text color='green'>{destination}</Text></Text>}
+    </>
   )
 }
 
