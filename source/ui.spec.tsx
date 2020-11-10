89f87880e141b0270d94808ab9878448deb3c17d
joe.wright <joe@joejag.com>
2020-11-10 23:22:51 +0000

    Fix tests

--- source/ui.spec.tsx
+++ source/ui.spec.tsx
@@ -3,14 +3,18 @@ import chalk from 'chalk'
 import { render } from 'ink-testing-library'
 import App from './ui'
 
-test('greet unknown user', () => {
+test('print out languages when nothing specified', () => {
   const { lastFrame } = render(<App />)
 
-  expect(lastFrame()).toBe(chalk`Hello, {green Stranger}`)
+  expect(lastFrame()).toContain(chalk`Please choose a language to create a starter for.`)
+  expect(lastFrame()).toContain(chalk`java`)
 })
 
-test('greet user with a name', () => {
-  const { lastFrame } = render(<App language='node' />)
+test('print out error on unknown language', () => {
+  const { lastFrame } = render(<App language='unknownthing' />)
 
-  expect(lastFrame()).toBe(chalk`Hello, {green Jane}`)
+  expect(lastFrame()).toContain(chalk`Sorry, {red unknownthing} is not supported.`)
+  expect(lastFrame()).toContain(chalk`java`)
 })
+
+test.todo('can generate a node project')
